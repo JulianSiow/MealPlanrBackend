c385c6dd1a0e7b61515e6b78f990645e64e18f30
Julian <Julian.K.Siow@gmail.com>
2020-08-03 13:02:04 +0200

    initial node.js and express setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
+//=========================== MIDDLEWARE ====================
+
+//CORS
+const corsOptions = {
+    origin: [`http://localhost:3000`],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Express Sessions
+app.use(session({
+    store: new MongoStore({ url: process.env.MONGO_URI }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2
+    }
+}));
+
+//===================== Routes ===============================
+
+app.get('/', (req, res) => {
+    res.send('<h1>MEAL PLANR</h1>');
+});
+
+app.listen(PORT, () => console.log(`Server connected at http://localhost:${PORT}`));
